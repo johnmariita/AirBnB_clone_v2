67bb056128fdd8c4ab9d12ed7563426321bba204
johnmariita <johnmariita421@gmail.com>
2024-04-29 22:33:33 +0300

    Setup mysql dev

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates a database user and assigns permissions to the user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
