c04b9d8b3a9688a2d19246c5028a1a22d4b96c97
johnmariita <johnmariita421@gmail.com>
2024-05-19 08:34:04 +0300

    setting up my servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,10 +5,10 @@ sudo apt-get -y install nginx
 sudo mkdir -p '/data/web_static/releases/test'
 sudo mkdir -p /data/web_static/shared
 sudo ln -sf '/data/web_static/releases/test/' /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo touch /data/web_static/releases/test/index.html
 new_string="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
 sudo sed -i "s/server_name _;/$new_string/" /etc/nginx/sites-enabled/default
 
