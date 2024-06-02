763b1eead2e7234a27862e8b99cbc7a10dba906c
johnmariita <johnmariita421@gmail.com>
2024-06-02 10:23:52 +0300

    hbnb routing

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,6 +4,7 @@ A minimal application using flask
 """
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """
-    Function that returns the message hello HBNB! to the browser
+    Function that returns the message hello HBNB to the browser
     """
     return "Hello HBNB!"
 
@@ -19,9 +20,18 @@ def hello():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """
-    Function that returns the message HBNB to the browser
+    Function that returns the message hbnb to the browser
     """
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Function that handles the routing for /c/<text>
+    """
+    text.replace("_", " ")
+    return f"C {escape(text)}"
 
 
 if __name__ == "__main__":
