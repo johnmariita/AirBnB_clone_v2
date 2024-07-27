0f14d2204ed8718d1a5b16bbb69878058d5d1d24
johnmariita <johnmariita421@gmail.com>
2024-07-27 12:08:18 +0300

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+minimal web app that listens on 0.0.0.0, port 5000
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    returns Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cFun(text):
+    """
+    Function that checks the route
+    after c and returns a string
+    """
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """
+    Function that checks the route
+    after python and returns a string
+    """
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
