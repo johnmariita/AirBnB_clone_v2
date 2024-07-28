e2559ab56ad2eedee479205c312d0a44d21a4d03
johnmariita <johnmariita421@gmail.com>
2024-07-28 12:38:05 +0300

    states lisy

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -20,5 +20,11 @@ def stateList():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
